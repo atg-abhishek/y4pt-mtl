367b3c5d90431b09bf2b5b4974efd72fdff4e29e
atg.abhishek <atg.abhishek@gmail.com>
2017-05-12 11:51:01 -0400

    fixed another error

--- backend/server.py
+++ backend/server.py
@@ -2,11 +2,11 @@ from flask import Flask, request
 import sys, requests
 from tinydb import TinyDB, Query
 
-app = Flask(__name__)
-db = TinyDB(DB_ADDRESS)
-
 HOST = None
 DB_ADDRESS = "db.json"
+
+app = Flask(__name__)
+db = TinyDB(DB_ADDRESS)
 
 if len(sys.argv)>1 and sys.argv[1] == "prod":
     HOST = '0.0.0.0'
